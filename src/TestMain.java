df454027098aab77259e50fcf2b2e7dc12ed2963
zachkova <zachkova@gmail.com>
2021-02-24 18:17:15 -0500

    Added Recieved Files

---
+++ src/TestMain.java
@@ -0,0 +1,39 @@
+
+import java.util.Properties;
+
+import model.*;
+
+public class TestMain
+{
+
+       public static void main(String[] args)
+       {
+          try
+          {
+			Book bk = new Book("1");
+			System.out.println(bk.toString());
+		  
+			Properties p = new Properties();
+			p.setProperty("bookTitle", "I hate Sandeep Mitra");
+			p.setProperty("author", "Kyle Adams");
+			p.setProperty("pubYear", "2020");
+			p.setProperty("status", "Active");
+			
+			Book bk2 = new Book(p);
+			bk2.update();
+		  
+		    Patron pat = new Patron("1");
+			System.out.println(pat.toString());
+		  
+	    
+	 
+          }
+          catch (Exception ex) 
+          {
+			System.out.println("Error in accessing Kyle stupid database: " + ex.toString());
+		  }
+
+		
+
+       }
+}
